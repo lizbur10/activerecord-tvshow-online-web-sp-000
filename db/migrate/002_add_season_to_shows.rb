40468624f30f2df608a4bb5ea403b2b58ec91d3e
Alison Reilly <reillyamr@gmail.com>
2019-03-08 19:08:22 +0000

    Done.

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,1 +1,1 @@
-class AddSeasonToShows < ActiveRecord::Migration  def change    add_column :shows, :season, :string  endend
+class AddSeasonToShows < ActiveRecord::Migration  def change     add_column :shows, :season, :string        endend
